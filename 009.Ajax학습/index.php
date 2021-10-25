b724499d0182a5e8e59237b6829093a7300bbbf5
KwonSsohyun <thgus4982@naver.com>
2021-10-25 14:10:40 +0900

    Update index.php

--- 009.Ajax학습/index.php
+++ 009.Ajax학습/index.php
@@ -18,11 +18,14 @@
 
         // 화면출력 -> echo 메시지
         echo "<h1>나의 첫 PHP페이지!</h1>"; 
-        /* http://creative103.dothome.co.kr/ ▶ 나옴 
-           http://creative103.dothome.co.kr/vogue/ ▶ 원래 이렇게 써야 나왔다.
+        /* http://creative103.dothome.co.kr/ ▶ 닷홈에서  기본 제공 도메인에 주소 붙혀서 들어가면 원래 안나왔는데, 해당 PHP 파일 만드니 첫화면 나옴 
+           http://creative103.dothome.co.kr/vogue/ ▶ 원래 이렇게 뒤에 써야 나왔다.
         */
 
-        echo "<h2>$link</h2>"; /* 변수 넣음 */
+        // 쌍따옴표안에 php변수를 바로 쓸 수 있음!
+        echo "<h2>$link</h2>"; /* 변수 넣음 
+        ▶ 이제 "보그바로가기" 누르면 보그제작한 페이지로 이동됨 
+           http://creative103.dothome.co.kr/vogue/ ▶ 해당페이지로 이동됨 */
 
     
     ?>
